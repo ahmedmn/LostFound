077d2ca63bb15c3b6ae1f1335c05c7c226166c47
savco <michal.savcinsky@gmail.com>
2016-11-18 12:56:21 +0100

    Created BeanMappingService + folders structure

---
+++ lostfound-service/src/main/java/com/LostFound/service/BeanMappingService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.LostFound.service;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.dozer.Mapper;
+
+
+/**
+ *
+ * @author Michal
+ */
+
+public interface BeanMappingService {
+	
+    public  <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass);
+
+    public  <T> T mapTo(Object u, Class<T> mapToClass);
+    public Mapper getMapper();
+}
