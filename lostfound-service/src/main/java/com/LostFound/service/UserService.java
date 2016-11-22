b8aecc19c61d4b198526777314416c955333d733
Martin Bokša <martin.boksa@gmail.com>
2016-11-22 22:16:47 +0100

    UserService implemented.

--- lostfound-service/src/main/java/com/LostFound/service/UserService.java
+++ lostfound-service/src/main/java/com/LostFound/service/UserService.java
@@ -7,12 +7,56 @@ package com.LostFound.service;
 
 import com.LostFound.entity.User;
 
+import java.util.List;
+
 /**
  *
- * @author Michal
+ * @author Bokos
  */
 public interface UserService {
 
-    public User findById(Long userId);
-   //todo 
+    /**
+     * Create method is used for insert information in table Users
+     *
+     * @param user object to be created
+     */
+    public void create(User user);
+
+    /**
+     * Delete method is used for delete user according to user id in table Users.
+     *
+     * @param user object to be deleted.
+     */
+    public void delete(User user);
+
+    /**
+     * Returns user corresponding to given user id.
+     *
+     * @param id id of user to be found
+     * @return user with given id
+     */
+    public User findById(Long id);
+
+    /**
+     * Returns user corresponding to given user name.
+     *
+     * @param name name of user to be found
+     * @return user with given name
+     */
+    public User findByName(String name);
+
+    /**
+     * Returns user corresponding to given user email.
+     *
+     * @param email email of user to be found
+     * @return user with given email
+     */
+    public User findByEmail(String email);
+
+    /**
+     * Returns information about all users.
+     *
+     * @return list of all users
+     */
+    public List<User> findAll();
 }
--- lostfound-service/src/main/java/com/LostFound/service/UserServiceImpl.java
+++ lostfound-service/src/main/java/com/LostFound/service/UserServiceImpl.java
@@ -1,15 +1,42 @@
 package com.LostFound.service;
 
+import com.LostFound.dao.UserDAO;
 import com.LostFound.entity.User;
 import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
+import java.util.List;
+
 /**
- * Only fake implementation of UserService because
- * project couldn't be compiled without no implementation of UserService
+ *
+ * @author Bokos
  */
 @Service
 public class UserServiceImpl implements UserService {
+    @Inject
+    private UserDAO userDao;
+
+    public void create(User user) {
+        userDao.create(user);
+    }
+
+    public void delete(User user) {
+        userDao.delete(user);
+    }
+
     public User findById(Long userId) {
-        return null;
+        return userDao.findById(userId);
+    }
+
+    public User findByName(String name) {
+        return userDao.findByName(name);
+    }
+
+    public User findByEmail(String email) {
+        return userDao.findByEmail(email);
+    }
+
+    public List<User> findAll() {
+        return userDao.findAll();
     }
 }
