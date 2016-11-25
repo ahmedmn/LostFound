75ef619dde134c4ae7fe340c201edd1c5421a2f0
Peter Kovac <petookovac@gmail.com>
2016-11-25 21:33:41 +0100

    recover from git push --force -> UserServiceTest

---
+++ lostfound-service/src/test/java/com/LostFound/service/UserServiceTest.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.LostFound.service;
+
+import com.LostFound.dao.ItemDAO;
+import com.LostFound.dao.PostDAO;
+import com.LostFound.dao.UserDAO;
+import com.LostFound.entity.Post;
+import com.LostFound.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.service.spi.ServiceException;
+import org.mockito.InjectMocks;
+import static org.mockito.Matchers.any;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Michal
+ * UserServiceTest class is used to test functionalities of User Service
+ */
+@ContextConfiguration(locations = "file:src/main/resources/spring-service.xml")
+public class UserServiceTest extends AbstractTestNGSpringContextTests {
+
+    @Mock
+    private UserDAO userDao;
+    
+    @Autowired
+    @InjectMocks
+    private UserService userService;
+    
+    @BeforeClass
+    public void setup() throws ServiceException {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    private User expectedUser;
+    
+    private List<User> expectedUserList = new ArrayList<User>();
+    
+    @Test 
+    public void create(){
+        //call service method
+        userService.create(expectedUser);
+
+        //verify
+        verify(userDao, times(1)).create(expectedUser);
+    }
+    
+    @Test
+    public void findById(){
+        //setup
+        Long idToFind = 10L;
+        //mock
+        when(userDao.findById(any(Long.class))).thenReturn(expectedUser);
+        //call service method
+        User actualUser = userService.findById(idToFind);
+        //verify
+        verify(userDao, times(1)).findById(idToFind);
+        //check
+        Assert.assertEquals(actualUser, expectedUser);
+    }
+    
+    @Test
+    public void findByName(){
+        //setup
+        String name = "Michal";
+        //mock
+        when(userDao.findByName(any(String.class))).thenReturn(expectedUser);
+        //call service method
+        User actualUser = userService.findByName(name);
+        //verify
+        verify(userDao, times(1)).findByName(name);
+        //check
+        Assert.assertEquals(actualUser, expectedUser);
+    }
+    
+    @Test
+    public void findByEmail(){
+        //setup
+        String email = "michal@mail.com";
+        //mock
+        when(userDao.findByEmail(any(String.class))).thenReturn(expectedUser);
+        //call service method
+        User actualUser = userService.findByEmail(email);
+        //verify
+        verify(userDao, times(1)).findByEmail(email);
+        //check
+        Assert.assertEquals(actualUser, expectedUser);
+    }
+    
+    @Test
+    public void findAll(){
+        //mock
+        when(userDao.findAll()).thenReturn(expectedUserList);
+        //call service method
+        List<User> actualUserList = userService.findAll();
+        //verify
+        verify(userDao, times(1)).findAll();
+        //check
+        Assert.assertEquals(actualUserList, expectedUserList);
+    }
+    
+    @Test
+    public void delete() {
+        
+    }
+}
